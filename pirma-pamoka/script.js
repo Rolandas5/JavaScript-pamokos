b6c289b594b73d7ceaa65d3fa8ca8ac61729d218
Rolandas5 <r.bendorius@gmail.com>
2024-10-30 21:45:37 +0200

    Initial comit

---
+++ pirma-pamoka/script.js
@@ -0,0 +1,25 @@
+// log
+console.log(2);
+console.log("Rolandas");
+console.log(1000000);
+// error
+console.error("Klaida");
+//clear
+console.clear();
+// Math
+console.log(10 + 10 <= 30);
+// Info
+console.info("Informacija");
+// Table
+console.table(["apple", "banana", "orange"]);
+// worn
+console.warn("Įspėjimas");
+// Time (padeda pamatouti kiek laiko užtrunka vykdyti kodą)
+console.time();
+// Kodas kurį skaičiuoja
+console.timeEnd();
+
+alert(2);
+alert(1);
+
+// document.getElementById("heading").innerHTML = 'Hello world!';
